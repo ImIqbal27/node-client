210973087e4fb4f2aeaf71c2cb50dec45ec287ed
Iqbal <iamhasan9501@gmail.com>
2023-03-11 20:10:19 +0600

    init

---
+++ src/components/AddUser.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+const AddUser = () => {
+  const [user, setUser] = useState({ name: "de-name", email: "de@fault.com" });
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:5000/users`, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknwledged) {
+          alert("user added");
+          e.target.reset();
+        }
+      });
+  };
+  const handleInputBlur = (event) => {
+    const value = event.target.value;
+    const field = event.target.name;
+    // console.log("va-", value, "fi-", field);
+    const newUser = { ...user };
+    newUser[field] = value;
+    setUser(newUser);
+    console.log(newUser);
+  };
+  return (
+    <div>
+      <h2>Please add a new user</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input
+          onBlur={handleInputBlur}
+          type="text"
+          name="name"
+          placeholder="type a name"
+          required
+        />{" "}
+        <br />
+        <input
+          onBlur={handleInputBlur}
+          type="email"
+          name="email"
+          placeholder="type a email"
+          required
+        />{" "}
+        <br />
+        <input
+          onBlur={handleInputBlur}
+          type="number"
+          name="phone"
+          placeholder="type a phone"
+          required
+        />{" "}
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ src/components/Home.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Home = () => {
+  const users = useLoaderData();
+  const [displayUsers, setDisplayUsers] = useState(users);
+  const handleDelete = (user) => {
+    const agree = window.confirm(`Are you sure to delete ${user.name} ?`);
+    if (agree) {
+      fetch(`http://localhost:5000/users/${user._id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount) {
+            alert(`User : ${user.name} deleted successfully`);
+            const remaining = displayUsers.filter((usr) => usr._id !== user.id);
+            setDisplayUsers(remaining);
+          }
+        });
+      // console.log(agree);// console.log(user);deletedCount
+    }
+  };
+  return (
+    <div>
+      <h3>Home Users :{displayUsers.length}</h3>
+      <div>
+        {displayUsers.map((user) => (
+          <p key={user._id}>
+            {user.name} {"#"}
+            {user.email}
+            {"#"}
+            {user.phone} {"#"}
+            {user?.namee}
+            {/* <button onClick={() => handleDelete(user._id)}>X</button> */}
+            <button onClick={() => handleDelete(user)}>X</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
